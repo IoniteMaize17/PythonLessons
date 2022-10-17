042ff9175ef475ce451841e77c342a4729ae14b4
Nguyen Duc <mduc0123@gmail.com>
2022-10-17 22:13:04 +0700

    My first change

---
+++ C2_1_main.py
@@ -0,0 +1,11 @@
+value = 100
+print(value)
+value = "fluffy the terrible"
+print(value)
+stringline = "teapot"
+number = 1100
+print(stringline)
+print(number)
+number + 150
+stuff = number
+print(stuff)
