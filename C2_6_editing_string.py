d47a72ab25a59daa89805f6c643ce986e9d15add
Nguyen Duc <mduc0123@gmail.com>
2022-10-18 21:18:46 +0700

    Change C2_6 and add C2_7

--- C2_6_editing_string.py
+++ C2_6_editing_string.py
@@ -1,9 +1,13 @@
 #string slicing
 bigstring = "auxiliaryemergencyfirepreventionsystem"
-bigstring2 = "abcxyz"
+bigstring2 = ""
 print(len(bigstring))
-print(bigstring[2])
+print(bigstring[len(bigstring)-1])
 print([bigstring][0]) #this makes the string 1 element, meaning cannot count other than 0 and -1
 
-names = ["a","b","c"]
-print(names[1])
+#slicing several characters
+print(bigstring[-3:40:2])#if end point exceed the last position, auto-adjust the end poin to last position
+print(bigstring[-3:-5:2])#if start point exceed end point, return empty string (not error)
+print(bigstring[-3:-7:-2])#to moon_walk(), use negative step
+print(bigstring[::-1])#this use the default value of start point and end point (can show the end point)
+print(len(bigstring2))
---
+++ C2_7_string_methods.py
@@ -0,0 +1,13 @@
+text = "I'm not an idiot!"
+number = "1515155151515"
+res = text.casefold().count("i",0,len(text))
+print(res)
+print(text.isalpha())
+print(text.replace("i","a",1))
+print("action")
+##string methods
+variable_1 = "Stringline!"
+variable_2 = "Llamas?"
+print(variable_1)
+value = input("Enter something ")
+print("Result is " + value)
